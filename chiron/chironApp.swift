6dfefabbf08fa99cd5726e42283b35e2c8d94626
hungrymonkey <boringmonkey@gmail.com>
2025-11-09 13:58:08 -0500

    Initial Commit

---
+++ chiron/chironApp.swift
@@ -0,0 +1,17 @@
+//
+//  chironApp.swift
+//  chiron
+//
+//  Created by Ted Chang on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct chironApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
